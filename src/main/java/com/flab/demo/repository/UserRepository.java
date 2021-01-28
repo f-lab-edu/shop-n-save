fc16a1dd267d576a90e3fe19daaddd44e75689f8
sanggu <tkdrn8578@naver.com>
2021-01-28 22:53:30 +0900

    feat: 사용자 정보를 저장하기 위한 UserRepository 추가

---
+++ src/main/java/com/flab/demo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.flab.demo.repository;
+
+import com.flab.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
