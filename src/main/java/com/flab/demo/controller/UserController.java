712a90312f2a943b1054041914296f3867177464
sanggu <tkdrn8578@naver.com>
2021-01-31 00:19:12 +0900

    refactor: mock handler 를 제거하고 UserService 에서 사용자 정보를 가져와서 해당 정보를 ResponseEntity 로 넘기도록 수정
     * ResponseEntity 를 사용하여 사용자에게 Http Status 를 OK 로 리턴한다.
     * body 에 사용자 정보를 담아 리턴한다.

--- src/main/java/com/flab/demo/controller/UserController.java
+++ src/main/java/com/flab/demo/controller/UserController.java
@@ -2,15 +2,23 @@ package com.flab.demo.controller;
 
 import com.flab.demo.domain.User;
 import com.flab.demo.dto.CreateUserRequestDto;
+import com.flab.demo.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/users")
-    public User create(@RequestBody CreateUserRequestDto user) {
-        return new User("sgkim94@github.com", "123456");
+    public ResponseEntity create(@RequestBody CreateUserRequestDto user) {
+        User savedUser = userService.save(user);
+        return ResponseEntity.ok(savedUser);
     }
 }
