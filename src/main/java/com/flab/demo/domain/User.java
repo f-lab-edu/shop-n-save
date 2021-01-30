c52799697df34e7e2624717b1b7f4838a3818964
sanggu <tkdrn8578@naver.com>
2021-01-31 00:23:58 +0900

    refactor: 기본 생성자를 @NoArgsConstructor(access = AccessLevel.PROTECTED) 로 추출
     * 내부에 선언하는 것보다 어노테이션으로 선언하는게 가독성이 좋다 생각하여 추출

--- src/main/java/com/flab/demo/domain/User.java
+++ src/main/java/com/flab/demo/domain/User.java
@@ -1,23 +1,31 @@
 package com.flab.demo.domain;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue
     private Long id;
 
     @Column
+    @Email
     private String email;
 
     @Column
     private String password;
 
-    protected User() {
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 
     public String getPassword() {
