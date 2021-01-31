d045314b59561d186156018e70bd0601c7eaf709
sanggu <tkdrn8578@naver.com>
2021-01-31 17:10:30 +0900

    refactor: 사용자 입력값을 dto 에서 valid 하기 위한 NotBlank 와 Email annotation 추가

--- src/main/java/com/flab/demo/dto/CreateUserRequestDto.java
+++ src/main/java/com/flab/demo/dto/CreateUserRequestDto.java
@@ -5,19 +5,34 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class CreateUserRequestDto {
+    @Email
+    @NotBlank(message = "이메일은 필수값입니다.")
     private String email;
+
+    @NotBlank(message = "패스워드는 필수값입니다.")
     private String password;
 
+    @NotBlank(message = "이름은 필수값입니다.")
+    private String name;
+
     @Builder
-    public CreateUserRequestDto(String email, String password) {
+    public CreateUserRequestDto(String email, String password, String name) {
         this.email = email;
         this.password = password;
+        this.name = name;
     }
 
     public User toEntity() {
-        return new User(this.email, this.password);
+        return User.builder()
+                .email(this.email)
+                .password(this.password)
+                .name(this.name)
+                .build();
     }
 }
