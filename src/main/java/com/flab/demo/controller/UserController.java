4141af9d5476f8e4b81faa628ff70485ec332813
sanggu <tkdrn8578@naver.com>
2021-01-31 15:03:13 +0900

    feat: login 을 하기 위한 mock endpoint 추가 및 Valid 추가
     * Valid 를 사용하여 사용자 정보의 유효성 검사를 진행한다.

--- src/main/java/com/flab/demo/controller/UserController.java
+++ src/main/java/com/flab/demo/controller/UserController.java
@@ -2,22 +2,32 @@ package com.flab.demo.controller;
 
 import com.flab.demo.domain.User;
 import com.flab.demo.dto.CreateUserRequestDto;
+import com.flab.demo.dto.LoginUserRequestDto;
 import com.flab.demo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
-    
+
     private UserService userService;
 
-    @PostMapping("/users")
-    public ResponseEntity create(@RequestBody CreateUserRequestDto user) {
+    @PostMapping
+    public ResponseEntity create(@Valid  @RequestBody CreateUserRequestDto user) {
         User savedUser = userService.save(user);
         return ResponseEntity.ok(savedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@Valid  @RequestBody LoginUserRequestDto user) {
+        return ResponseEntity.ok().build();
     }
 }
