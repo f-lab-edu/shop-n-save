66e6835d6ffbcfe12bb23977a4f2179559b63784
sanggu <tkdrn8578@naver.com>
2021-01-31 17:03:34 +0900

    refactor: login 전에 사용자를 생성하기 위한 로직을 공통으로 추출
     * test code 를 동시에 돌렸을 때 발생하는 사용자가 충돌되는 문제를 방지하기 위한 beforeEach 에 deleteAll 추가

--- src/test/java/com/flab/demo/controller/UserControllerTest.java
+++ src/test/java/com/flab/demo/controller/UserControllerTest.java
@@ -3,6 +3,8 @@ package com.flab.demo.controller;
 import com.flab.demo.domain.User;
 import com.flab.demo.dto.CreateUserRequestDto;
 import com.flab.demo.dto.LoginUserRequestDto;
+import com.flab.demo.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,18 +25,19 @@ public class UserControllerTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("올바른 email 과 password 를 입력받은 경우 User 테이블에 저장되며 User 정보와 함께 ok status 를 리턴한다.")
-    void createUser() {
+    void create_user_test() {
         // when
-        User savedUser = webTestClient.post()
-                .uri(USERS_BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(TEST_USER), CreateUserRequestDto.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(User.class)
-                .returnResult().getResponseBody();
+        User savedUser = creatUser(CREATE_USER_REQUEST_DTO);
 
         // then
         assertThat(savedUser.getEmail()).isEqualTo(TEST_EMAIL);
@@ -43,25 +46,36 @@ public class UserControllerTest {
 
     @Test
     @DisplayName("회원가입된 email 과 password 를 입력받아 로그인 시도하는 경우 해당 회원 ID 를 ok status 와 함께 리턴한다.")
-    void login() {
+    void login_test() {
         // given
+        creatUser(CREATE_USER_REQUEST_DTO);
+
         LoginUserRequestDto user = LoginUserRequestDto.builder()
                 .email(TEST_EMAIL)
                 .password(TEST_PASSWORD)
                 .build();
 
         // when
-        User savedUser = webTestClient.post()
+        webTestClient.post()
                 .uri(USERS_BASE_URL + "/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(user), LoginUserRequestDto.class)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(User.class)
-                .returnResult().getResponseBody();
-
-        // then
-        assertThat(savedUser.getId()).isNotNull();
+                .returnResult()
+                .getResponseBody();
     }
 
+    private User creatUser(CreateUserRequestDto request) {
+        return webTestClient.post()
+                .uri(USERS_BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CreateUserRequestDto.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(User.class)
+                .returnResult()
+                .getResponseBody();
+    }
 }
