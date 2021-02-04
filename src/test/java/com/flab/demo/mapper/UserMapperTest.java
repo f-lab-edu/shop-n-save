4143655575133cf6ba87be39ea6b9f802ac691aa
sanggu <tkdrn8578@naver.com>
2021-02-04 22:29:26 +0900

    refactor: jpa -> mybatis 로 변경함에 따른 UserMapper Test 코드 수정

--- src/test/java/com/flab/demo/mapper/UserMapperTest.java
+++ src/test/java/com/flab/demo/mapper/UserMapperTest.java
@@ -1,44 +1,46 @@
-package com.flab.demo.repository;
+package com.flab.demo.mapper;
 
 import com.flab.demo.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.TransactionSystemException;
 
 import static com.flab.demo.fixture.UserFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-class UserRepositoryTest {
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserMapperTest {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserMapper userMapper;
+
+    @Test
+    @DisplayName("User 정보를 email, password, name 을 입력받아 저장한다.")
+    void save_test() {
+        //given
+        int addCount = userMapper.save(TEST_USER);
+
+        //then
+        assertThat(addCount).isEqualTo(1);
+    }
 
     @Test
     @DisplayName("User 정보를 email 로 검색할 상황일 때 email 에 해당하는 User 를 리턴한다.")
-    void findByEmail() {
+    void findByEmail_test() {
         //given
-        User savedUser = userRepository.save(TEST_USER);
+        userMapper.save(TEST_USER);
 
         //when
-        User findUser = userRepository.findByEmail(savedUser.getEmail());
+        User findUser = userMapper.findByEmail(TEST_EMAIL);
 
         //then
         assertThat(findUser.getEmail()).isEqualTo(TEST_EMAIL);
         assertThat(findUser.getPassword()).isEqualTo(TEST_PASSWORD);
-    }
-
-    @Test
-    @DisplayName("사용자 정보 저장 시 email 형식이 아닌 경우 예외가 발생된다.")
-    void save_not_valid_email() {
-        assertThrows(TransactionSystemException.class,
-                () -> userRepository.save(new User("abc", "234565"))
-        );
     }
 }
