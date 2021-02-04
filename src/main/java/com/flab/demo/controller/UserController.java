5c391097ceb72288cffcd19dc9c4a3edfb76526a
sanggu <tkdrn8578@naver.com>
2021-02-04 22:53:25 +0900

    feat: DTO 의 필수 값 체크를 위한 @Valid 추가

--- src/main/java/com/flab/demo/controller/UserController.java
+++ src/main/java/com/flab/demo/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.flab.demo.controller;
 
-import com.flab.demo.domain.User;
 import com.flab.demo.dto.CreateUserRequestDto;
 import com.flab.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -16,7 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity create(@RequestBody CreateUserRequestDto user) {
+    public ResponseEntity create(@Valid @RequestBody CreateUserRequestDto user) {
         userService.save(user);
         return ResponseEntity.ok().build();
     }
