75a06e5d5a94b41be6575a6055179154e8eea053
sanggu <tkdrn8578@naver.com>
2021-02-04 22:29:37 +0900

    refactor: UserRepository 제거

--- src/main/java/com/flab/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flab.demo.repository;
-
-import com.flab.demo.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);
-}
