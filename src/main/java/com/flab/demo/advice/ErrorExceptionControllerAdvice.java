8144cbf2533b6020c12d3f992f33888ad2cb891e
sanggu <tkdrn8578@naver.com>
2021-02-04 22:53:44 +0900

    refactor: Exception 발생 시 500 에러를 리턴하도록 수정

--- src/main/java/com/flab/demo/advice/ErrorExceptionControllerAdvice.java
+++ src/main/java/com/flab/demo/advice/ErrorExceptionControllerAdvice.java
@@ -1,11 +1,12 @@
 package com.flab.demo.advice;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
 @ControllerAdvice
@@ -13,7 +14,7 @@ public class ErrorExceptionControllerAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity handleMethodException(Exception e) {
         log.error(String.valueOf(e.getCause()));
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -25,14 +26,14 @@ public class ErrorExceptionControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity handleRuntimeException(RuntimeException e) {
-        log.error(String.valueOf(e.getCause()));
+        log.error(String.valueOf(e.getMessage()));
 
         return ResponseEntity.unprocessableEntity().body(e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity handleIllegalArgumentException(IllegalArgumentException e) {
-        log.error(String.valueOf(e.getCause()));
+        log.error(String.valueOf(e.getMessage()));
 
         return ResponseEntity.unprocessableEntity().body(e.getMessage());
     }
