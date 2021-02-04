04682c5f0e07c36f565f36d1042a6ab5d8103aec
sanggu <tkdrn8578@naver.com>
2021-02-04 23:19:09 +0900

    feat: password 암호화를 위한 sha256 util 추가

---
+++ src/main/java/com/flab/demo/utils/SHAUtil.java
@@ -0,0 +1,31 @@
+package com.flab.demo.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHAUtil {
+    public static final String SHA_256 = "SHA-256";
+
+    public static String digest(String input, String algorithm) {
+        MessageDigest md;
+
+        try {
+            md = MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        byte[] digest = md.digest(input.getBytes());
+        return bytesToHex(digest);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+
+        for (byte data : bytes) {
+            sb.append(String.format("%02x", data));
+        }
+
+        return sb.toString();
+    }
+}
