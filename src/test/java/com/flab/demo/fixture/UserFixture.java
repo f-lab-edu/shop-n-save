399346b4833ca422371fe64788ae500540510f62
sanggu <tkdrn8578@naver.com>
2021-02-04 22:26:33 +0900

    refactor: 생성자로 User 와 CreateUserRequestDto 를 생성한 것을 builder 로 수정

--- src/test/java/com/flab/demo/fixture/UserFixture.java
+++ src/test/java/com/flab/demo/fixture/UserFixture.java
@@ -6,8 +6,14 @@ import com.flab.demo.dto.CreateUserRequestDto;
 public class UserFixture {
     public static final String TEST_EMAIL = "sgkim94@github.com";
     public static final String TEST_PASSWORD = "123456";
+    public static final String TEST_NAME = "김상구";
 
-    public static final User TEST_USER = new User(TEST_EMAIL, TEST_PASSWORD);
+    public static final User TEST_USER = User.builder()
+            .email(TEST_EMAIL)
+            .password(TEST_PASSWORD)
+            .name(TEST_NAME)
+            .build();
+
     public static final CreateUserRequestDto CREATE_USER_REQUEST_DTO
-            = new CreateUserRequestDto(TEST_EMAIL, TEST_PASSWORD);
+            = new CreateUserRequestDto(TEST_EMAIL, TEST_NAME, TEST_PASSWORD);
 }
