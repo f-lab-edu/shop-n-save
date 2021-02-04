6cbd3026ae0133a0619aab8e26d26bacebbbc7e4
sanggu <tkdrn8578@naver.com>
2021-02-04 22:30:37 +0900

    refactor: jpa -> mybatis 로 변경에 따른 UserServiceTest 변경

--- src/test/java/com/flab/demo/security/UserServiceTest.java
+++ src/test/java/com/flab/demo/security/UserServiceTest.java
@@ -2,7 +2,7 @@ package com.flab.demo.security;
 
 import com.flab.demo.domain.User;
 import com.flab.demo.dto.CreateUserRequestDto;
-import com.flab.demo.repository.UserRepository;
+import com.flab.demo.mapper.UserMapper;
 import com.flab.demo.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +15,7 @@ import static com.flab.demo.fixture.UserFixture.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,28 +25,42 @@ class UserServiceTest {
     private UserService userService;
 
     @Mock
-    private UserRepository userRepository;
+    private UserMapper userMapper;
 
     @Test
     @DisplayName("사용자 회원가입 시 존재하는 email 인 상황일 때 IllegalArgumentException 예외가 발생한다.")
     void save_fail_when_exist_user_email() {
-        when(userRepository.findByEmail(TEST_EMAIL)).thenReturn(new User(TEST_EMAIL, TEST_PASSWORD));
+        when(userMapper.findByEmail(TEST_EMAIL)).thenReturn(TEST_USER);
 
         String errorMessage = assertThrows(IllegalArgumentException.class,
-                () -> userService.save(new CreateUserRequestDto(TEST_EMAIL, TEST_PASSWORD))
+                () -> userService.save(CREATE_USER_REQUEST_DTO)
         ).getMessage();
 
         assertThat(errorMessage).isEqualTo("등록된 사용자입니다.");
     }
 
     @Test
+    @DisplayName("사용자 회원가입 시 존재하는 email 인 상황일 때 IllegalArgumentException 예외가 발생한다.")
+    void save_fail_when_fail_add_user() {
+        when(userMapper.findByEmail(TEST_EMAIL)).thenReturn(null);
+        when(userMapper.save(any())).thenReturn(0);
+
+        String errorMessage = assertThrows(RuntimeException.class,
+                () -> userService.save(CREATE_USER_REQUEST_DTO)
+        ).getMessage();
+
+        assertThat(errorMessage).isEqualTo("사용자 정보를 저장에 실패하였습니다.");
+    }
+
+
+    @Test
     @DisplayName("올바른 형태의 email 과 password 를 입력받은 경우 User 테이블에 저장된 후 User 를 리턴한다.")
     void save() {
-        when(userRepository.save(any())).thenReturn(TEST_USER);
+        when(userMapper.save(any())).thenReturn(1);
 
-        User savedUser = userService.save(CREATE_USER_REQUEST_DTO);
+        userService.save(CREATE_USER_REQUEST_DTO);
 
-        assertThat(savedUser.getEmail()).isEqualTo(TEST_EMAIL);
-        assertThat(savedUser.getPassword()).isEqualTo(TEST_PASSWORD);
+        verify(userMapper).findByEmail(any());
+        verify(userMapper).save(any());
     }
 }
