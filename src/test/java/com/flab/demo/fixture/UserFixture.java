fa98282d5346f6e689c9eace2003899c710ac4cb
sanggu <tkdrn8578@naver.com>
2021-02-05 23:33:52 +0900

    feat: HASH_PASSWORD 를 포함하는 User 테스트 fixture 추가

--- src/test/java/com/flab/demo/fixture/UserFixture.java
+++ src/test/java/com/flab/demo/fixture/UserFixture.java
@@ -2,10 +2,12 @@ package com.flab.demo.fixture;
 
 import com.flab.demo.domain.User;
 import com.flab.demo.dto.CreateUserRequestDto;
+import com.flab.demo.hash.Sha256;
 
 public class UserFixture {
     public static final String TEST_EMAIL = "sgkim94@github.com";
     public static final String TEST_PASSWORD = "123456";
+    public static final String TEST_HASH_PASSWORD = new Sha256().digest(TEST_PASSWORD, Sha256.SHA_256);
     public static final String TEST_NAME = "김상구";
 
     public static final User TEST_USER = User.builder()
@@ -13,6 +15,13 @@ public class UserFixture {
             .password(TEST_PASSWORD)
             .name(TEST_NAME)
             .build();
+
+    public static final User TEST_USER_WITH_HASH_PASSWORD = User.builder()
+            .email(TEST_EMAIL)
+            .password(TEST_HASH_PASSWORD)
+            .name(TEST_NAME)
+            .build();
+
 
     public static final CreateUserRequestDto CREATE_USER_REQUEST_DTO
             = new CreateUserRequestDto(TEST_EMAIL, TEST_NAME, TEST_PASSWORD);
