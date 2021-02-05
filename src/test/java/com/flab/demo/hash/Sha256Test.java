e6a1c5ac78f3d15a2ff121dc5cfd9fddbb2ce2d3
sanggu <tkdrn8578@naver.com>
2021-02-05 23:32:45 +0900

    refactor: SHAUtil -> Sha256 으로 변경에 따른 테스트 클래스 변경
     * https://google.github.io/styleguide/javaguide.html#s5.3-camel-case 을 참고하여 SHA -> Sha 로 변경

--- src/test/java/com/flab/demo/hash/Sha256Test.java
+++ src/test/java/com/flab/demo/hash/Sha256Test.java
@@ -1,20 +1,27 @@
-package com.flab.demo.utils;
+package com.flab.demo.hash;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import static com.flab.demo.fixture.UserFixture.TEST_PASSWORD;
-import static com.flab.demo.utils.SHAUtil.SHA_256;
-import static com.flab.demo.utils.SHAUtil.digest;
+import static com.flab.demo.hash.Sha256.SHA_256;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThrows;
 
-class SHAUtilTest {
+public class Sha256Test {
+
+    private Hash hash;
+
+    @Before
+    public void setUp() {
+        hash = new Sha256();
+    }
 
     @Test
     @DisplayName("입력값을 받아 SHA256 로 암호화하여 리턴한다.")
-    void digest_test() {
-        String encryptPassword = digest(TEST_PASSWORD, SHA_256);
+    public void digest_test() {
+        String encryptPassword = hash.digest(TEST_PASSWORD, SHA_256);
 
         assertThat(encryptPassword).isNotBlank();
         assertThat(encryptPassword).isNotEqualTo(TEST_PASSWORD);
@@ -22,9 +29,9 @@ class SHAUtilTest {
 
     @Test
     @DisplayName("알 수없는 알고리즘을 입력받아 시도하는 경우 예외처리가 발생한다.")
-    void digest_fail_test() {
+    public void digest_fail_test() {
         assertThrows(IllegalArgumentException.class,
-                () -> digest(TEST_PASSWORD, "unknown")
+                () -> hash.digest(TEST_PASSWORD, "unknown")
         );
     }
 }
