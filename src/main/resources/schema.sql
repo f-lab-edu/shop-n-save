c5df2bdc033a75dcbc5687529b840630c0f2647d
sanggu <tkdrn8578@naver.com>
2021-02-08 22:54:50 +0900

    refactor: H2 -> Mysql 로 설정 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,12 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user
 (
-    id          IDENTITY        PRIMARY KEY,
-    email       VARCHAR(255)    NOT NULL,
-    name        VARCHAR(255)    NOT NULL,
-    password    VARCHAR(255)    NOT NULL
+    id BIGINT AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+
+    CONSTRAINT user_pk
+    PRIMARY KEY (id)
 );
+
