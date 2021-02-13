61f0e9e9adf6019443fa10a1a18410a2784594c0
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-02-14 04:43:07 +0900

    MethodArgumentNotValidation 발생시 공통적으로 사용자에게 Response return

--- src/main/java/com/flab/demo/exception/GlobalExceptionController.java
+++ src/main/java/com/flab/demo/exception/GlobalExceptionController.java
@@ -1,4 +1,20 @@
 package com.flab.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
 public class GlobalExceptionController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ObjectError>> handleValidationExceptions(BindingResult bindingResult) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getAllErrors());
+    }
 }
