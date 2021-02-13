1a9b41b299c0fda706cf44ff644c7287b7267169
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-02-14 04:41:47 +0900

    refactor: Member Validation 어노테이션 설정

--- src/main/java/com/flab/demo/domain/Member.java
+++ src/main/java/com/flab/demo/domain/Member.java
@@ -2,6 +2,9 @@ package com.flab.demo.domain;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -10,7 +13,14 @@ import lombok.*;
 public class Member {
 
     private Long id;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
     private String name;
 }
