ece1bdec603350e81b6159ae5e1be4ae01471f8d
sanggu <tkdrn8578@naver.com>
2021-02-20 23:30:25 +0900

    refactor: encryptPassword 를 User domain 이 역할을 가지도록 수정

--- src/main/java/com/flab/demo/domain/User.java
+++ src/main/java/com/flab/demo/domain/User.java
@@ -1,11 +1,14 @@
 package com.flab.demo.domain;
 
+import com.flab.demo.security.Cryptographic;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
+
+import static com.flab.demo.security.Sha256.SHA_256;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -24,5 +27,9 @@ public class User {
         this.email = email;
         this.password = password;
         this.name = name;
+    }
+
+    public static String encryptPassword(String password, Cryptographic cryptographic) {
+        return cryptographic.digest(password, SHA_256);
     }
 }
