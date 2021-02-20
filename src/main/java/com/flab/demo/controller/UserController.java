34c28649483386484bede0d3e46a2cdd42fae617
sanggu <tkdrn8578@naver.com>
2021-02-20 23:30:51 +0900

    feat: 사용자가 로그인을 하기 위한 endpoint 추가

--- src/main/java/com/flab/demo/controller/UserController.java
+++ src/main/java/com/flab/demo/controller/UserController.java
@@ -1,29 +1,41 @@
 package com.flab.demo.controller;
 
-import com.flab.demo.domain.User;
 import com.flab.demo.dto.CreateUserRequestDto;
-import com.flab.demo.security.Sha256;
+import com.flab.demo.dto.LoginUserRequestDto;
 import com.flab.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import static com.flab.demo.controller.ResponseEntityGroup.OK_RESPONSE_ENTITY;
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
+    private static final String LOGIN_USER_ID_SESSION_KEY = "userId";
 
     private final UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity create(@Valid @RequestBody CreateUserRequestDto user) {
-        User hashUser = userService.convertToUserByHashPassword(user, new Sha256());
-        userService.save(hashUser);
+        userService.save(user);
+
         return OK_RESPONSE_ENTITY.getEntity();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@Valid @RequestBody LoginUserRequestDto user, HttpServletRequest request) {
+        Long loginUserId = userService.login(user);
+
+        request.setAttribute(LOGIN_USER_ID_SESSION_KEY, loginUserId);
+
+        return ResponseEntity.ok(loginUserId);
     }
 }
