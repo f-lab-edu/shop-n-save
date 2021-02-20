2c0bd31cf71c95a1a5e6909a44e19c285d4ae4fb
sanggu <tkdrn8578@naver.com>
2021-02-20 23:33:24 +0900

    refactor: password encrypt 를 생성자에 주입 시 진행하여 할당하도록 수정

--- src/main/java/com/flab/demo/dto/CreateUserRequestDto.java
+++ src/main/java/com/flab/demo/dto/CreateUserRequestDto.java
@@ -1,12 +1,15 @@
 package com.flab.demo.dto;
 
 import com.flab.demo.domain.User;
+import com.flab.demo.security.Sha256;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+
+import static com.flab.demo.domain.User.encryptPassword;
 
 @Getter
 @NoArgsConstructor
@@ -24,14 +27,14 @@ public class CreateUserRequestDto {
     @Builder
     public CreateUserRequestDto(String email, String password, String name) {
         this.email = email;
-        this.password = password;
+        this.password = encryptPassword(password, new Sha256());
         this.name = name;
     }
 
-    public User toEntity(String encryptPassword) {
+    public User toEntity() {
         return User.builder()
                 .email(this.email)
-                .password(encryptPassword)
+                .password(this.password)
                 .name(this.name)
                 .build();
     }
