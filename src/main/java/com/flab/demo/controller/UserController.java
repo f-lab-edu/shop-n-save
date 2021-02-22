fc9751b6cc22630d3d9d721b7fcaac69791f9331
sanggu <tkdrn8578@naver.com>
2021-02-22 22:24:01 +0900

    refactor: 사용자의 login id 를 session 에 저장하는 부분을 캡슐화하여 수정
     * 로그인 시 UserController 는 session 에 저장하는 로직에 대해 알지 못해도(알필요가 없다) 저장할 수 있어야 한다 - 캡슐화

--- src/main/java/com/flab/demo/controller/UserController.java
+++ src/main/java/com/flab/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.flab.demo.controller;
 
 import com.flab.demo.dto.CreateUserRequestDto;
 import com.flab.demo.dto.LoginUserRequestDto;
+import com.flab.demo.security.HttpSessionUtil;
 import com.flab.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import static com.flab.demo.controller.ResponseEntityGroup.OK_RESPONSE_ENTITY;
@@ -19,8 +20,6 @@ import static com.flab.demo.controller.ResponseEntityGroup.OK_RESPONSE_ENTITY;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private static final String LOGIN_USER_ID_SESSION_KEY = "userId";
-
     private final UserService userService;
 
     @PostMapping
@@ -31,10 +30,10 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(@Valid @RequestBody LoginUserRequestDto user, HttpServletRequest request) {
+    public ResponseEntity login(@Valid @RequestBody LoginUserRequestDto user, HttpSession session) {
         Long loginUserId = userService.login(user);
 
-        request.setAttribute(LOGIN_USER_ID_SESSION_KEY, loginUserId);
+        HttpSessionUtil.addUserSession(session, loginUserId);
 
         return ResponseEntity.ok(loginUserId);
     }
