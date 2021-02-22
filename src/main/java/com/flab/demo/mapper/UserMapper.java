4a59f52f3f978ed7eff1c52e380f2dc52c3c67bf
sanggu <tkdrn8578@naver.com>
2021-02-22 21:59:58 +0900

    feat: 로그인 시 이메일과 패스워드로 사용자 검색을 위한 findByEmailAndPassword 추가

--- src/main/java/com/flab/demo/mapper/UserMapper.java
+++ src/main/java/com/flab/demo/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.flab.demo.mapper;
 
 import com.flab.demo.domain.User;
+import com.flab.demo.dto.LoginUserRequestDto;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -8,4 +9,5 @@ public interface UserMapper {
 
     int save(User user);
     User findByEmail(String email);
+    User findByEmailAndPassword(LoginUserRequestDto dto);
 }
