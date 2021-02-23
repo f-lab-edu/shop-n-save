3b41a7e82d601662f0200e04c3886ff0c61a3249
sanggu <tkdrn8578@naver.com>
2021-02-23 23:49:56 +0900

    refactor: httpSessionUtil 에서 bean 으로 사용하기 위해 등록

--- src/main/java/com/flab/demo/security/Cryptographic.java
+++ src/main/java/com/flab/demo/security/Cryptographic.java
@@ -1,5 +1,8 @@
 package com.flab.demo.security;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface Cryptographic {
     String digest(String value, String algorithm);
 }
