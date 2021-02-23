8e66f0d2bf95e9ae41e2df665ce0699c514e581e
sanggu <tkdrn8578@naver.com>
2021-02-23 23:45:24 +0900

    refactor: HttpSessionUtil 을 static 이 아닌 주입받아서 테스트하도록 수정

--- src/test/java/com/flab/demo/security/HttpSessionUtilTest.java
+++ src/test/java/com/flab/demo/security/HttpSessionUtilTest.java
@@ -1,5 +1,6 @@
 package com.flab.demo.security;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpSession;
@@ -8,6 +9,13 @@ import static com.flab.demo.security.HttpSessionUtil.LOGIN_USER_ID_SESSION_KEY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HttpSessionUtilTest {
+    private AuthenticationService httpSessionUtil;
+
+    @BeforeEach
+    void setUp() {
+        httpSessionUtil = new HttpSessionUtil();
+    }
+
     @Test
     @DisplayName("사용자가 로그인 성공 시 해당 ID 를 session 에 저장을 저장한다.")
     void addUserSession_test() {
@@ -16,7 +24,7 @@ class HttpSessionUtilTest {
         MockHttpSession request = new MockHttpSession();
 
         //when
-        HttpSessionUtil.addUserSession(request, loginUserId);
+        httpSessionUtil.login(request, loginUserId);
 
         //then
         assertThat(request.getAttribute(LOGIN_USER_ID_SESSION_KEY)).isEqualTo(loginUserId);
