7beeb3783d10d0747ea43f71d4dd126ad20bc6b9
sanggu <tkdrn8578@naver.com>
2021-02-23 23:51:22 +0900

    refactor: PasswordEncoder 의 빈을 주입받아서 저장 및 검색 시 encode 하여 진행하도록 수정

--- src/main/java/com/flab/demo/service/UserService.java
+++ src/main/java/com/flab/demo/service/UserService.java
@@ -5,6 +5,7 @@ import com.flab.demo.dto.CreateUserRequestDto;
 import com.flab.demo.dto.LoginUserRequestDto;
 import com.flab.demo.exception.NotAuthenticationException;
 import com.flab.demo.mapper.UserMapper;
+import com.flab.demo.security.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public void save(CreateUserRequestDto user) {
         User registerUser = userMapper.findByEmail(user.getEmail());
@@ -21,18 +23,22 @@ public class UserService {
             throw new IllegalArgumentException("등록된 사용자입니다.");
         }
 
-        int addCount = userMapper.save(user.toEntity());
+        String encodedPassword = passwordEncoder.encryptPasswordBySha256(user.getPassword());
+
+        int addCount = userMapper.save(user.toEntity(encodedPassword));
 
         if (addCount == 0) {
             throw new RuntimeException("사용자 정보를 저장에 실패하였습니다.");
         }
     }
 
-    public Long login(LoginUserRequestDto user) {
-        User registerUser = userMapper.findByEmailAndPassword(user);
+    public Long findByEmailAndPassword(LoginUserRequestDto user) {
+        String encodedPassword = passwordEncoder.encryptPasswordBySha256(user.getPassword());
+
+        User registerUser = userMapper.findByEmailAndPassword(user.toDto(encodedPassword));
 
         if (registerUser == null) {
-            throw new NotAuthenticationException("로그인 사용자 정보가 올바르지 않습니다.");
+            throw new NotAuthenticationException("사용자 정보가 존재하지 않습니다.");
         }
 
         return registerUser.getId();
