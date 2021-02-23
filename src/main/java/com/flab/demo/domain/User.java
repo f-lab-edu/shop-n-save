836f8e3fd7822b1515e34b13f983325b93844b38
sanggu <tkdrn8578@naver.com>
2021-02-23 23:50:32 +0900

    refactor: user 가 가지고 있는 encryptPassword 를 PasswordEncoder 로 위임

--- src/main/java/com/flab/demo/domain/User.java
+++ src/main/java/com/flab/demo/domain/User.java
@@ -1,14 +1,11 @@
 package com.flab.demo.domain;
 
-import com.flab.demo.security.Cryptographic;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
-
-import static com.flab.demo.security.Sha256.SHA_256;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -27,9 +24,5 @@ public class User {
         this.email = email;
         this.password = password;
         this.name = name;
-    }
-
-    public static String encryptPassword(String password, Cryptographic cryptographic) {
-        return cryptographic.digest(password, SHA_256);
     }
 }
