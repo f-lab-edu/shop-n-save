d52a56ff127a90d5cf2dc74a45b64e19fdc0750a
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-02-26 17:00:11 +0900

    사용자 정의 예외 추가(로그인 인증 예외)

--- src/main/java/com/flab/demo/exception/UserAuthenticationFailException.java
+++ src/main/java/com/flab/demo/exception/UserAuthenticationFailException.java
@@ -1,4 +1,8 @@
 package com.flab.demo.exception;
 
-public class UserAuthenticationFailException {
+public class UserAuthenticationFailException extends RuntimeException{
+
+    public UserAuthenticationFailException(String message) {
+        super(message);
+    }
 }
