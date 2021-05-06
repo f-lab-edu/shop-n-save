2dbb913ed6e64431e5cf55bbb459ed36c0a7f0b1
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-05-07 01:51:25 +0900

    refactor: 로그인 기능 테스트 코드 수정

--- src/test/java/com/flab/demo/system/HttpSessionAuthentificationTest.java
+++ src/test/java/com/flab/demo/system/HttpSessionAuthentificationTest.java
@@ -1,37 +1,61 @@
 package com.flab.demo.system;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flab.demo.domain.Member;
 import com.flab.demo.exception.UserAuthenticationFailException;
 import com.flab.demo.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @Transactional
+@AutoConfigureMockMvc
 class HttpSessionAuthentificationTest {
 
-    @Autowired
-    private SessionAuthentification authentification;
+    private Member member;
+
+    @BeforeEach
+    public void setUp() {
+        member = new Member().builder()
+                .email("test1223@test")
+                .password("pw")
+                .name("testName")
+                .build();
+    }
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private Authentification authentification;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     public void 등록되지않은_사용자_로그인_예외() {
         // given
         Member member1 = new Member().builder()
-                .email("test@abc")
+                .email("unknown@abc.aaa")
                 .password("1234")
                 .name("mina")
                 .build();
-        MockHttpSession session = new MockHttpSession();
-
         // when
         UserAuthenticationFailException e = assertThrows(UserAuthenticationFailException.class, () -> authentification.login(member1));
 
@@ -43,18 +67,18 @@ class HttpSessionAuthentificationTest {
     public void 비밀번호_오입력_사용자_로그인_예외() {
         // given
         Member member1 = new Member().builder()
-                .email("test@abc")
+                .email("test1243@abc")
                 .password("1234")
                 .name("mina")
                 .build();
 
         Member member2 = new Member().builder()
-                .email("test@abc")
+                .email("test1243@abc")
                 .password("4321")
                 .name("mina")
                 .build();
+
         memberService.join(member1);
-        MockHttpSession session = new MockHttpSession();
 
         // when
         UserAuthenticationFailException e = assertThrows(UserAuthenticationFailException.class, () -> authentification.login(member2));
@@ -64,23 +88,18 @@ class HttpSessionAuthentificationTest {
     }
 
     @Test
-    public void 정상_사용자_로그인() {
+    public void 정상_사용자_로그인() throws Exception {
         // given
-        Member member1 = new Member().builder()
-                .email("test@abc")
-                .password("1234")
-                .name("mina")
-                .build();
+        memberService.join(member);
 
-        memberService.join(member1);
-        MockHttpSession session = new MockHttpSession();
+        String jsonString = objectMapper.writeValueAsString(member);
 
         // when
-        authentification.login(member1);
-
-        // then
-        // Member sessionMember = (Member)session.getAttribute(authentification.LOGIN);
-        // assertThat(sessionMember).isNotNull();
-        // assertThat(sessionMember.getEmail()).isEqualTo(member1.getEmail());
+        mockMvc.perform(post("/members/login")
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().exists(HttpHeaders.SET_COOKIE));
     }
 }
