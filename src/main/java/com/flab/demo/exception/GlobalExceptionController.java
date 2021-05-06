ab3484bddb9547b6f4bb3216bf5c2c0359ac9be1
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-05-07 00:57:25 +0900

    refactor: 에러 메시지 반환부 수정

--- src/main/java/com/flab/demo/exception/GlobalExceptionController.java
+++ src/main/java/com/flab/demo/exception/GlobalExceptionController.java
@@ -3,19 +3,25 @@ package com.flab.demo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<ObjectError>> handleValidationExceptions(BindingResult bindingResult) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getAllErrors());
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+        bindingResult.getAllErrors().forEach(c -> errors.put(((FieldError)c).getField() , c.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
