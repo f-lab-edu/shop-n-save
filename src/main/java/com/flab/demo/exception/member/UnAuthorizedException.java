cb51ecc29113cbdbcbb78bd0e887f448ddbc0773
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-06-02 02:26:04 +0900

    add: 권한 관련 Exception 추가

--- src/main/java/com/flab/demo/exception/member/UnAuthorizedException.java
+++ src/main/java/com/flab/demo/exception/member/UnAuthorizedException.java
@@ -1,4 +1,11 @@
 package com.flab.demo.exception.member;
 
-public class UnAuthorizedException {
+import com.flab.demo.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class UnAuthorizedException extends BusinessException {
+
+    public UnAuthorizedException() {
+        super("인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED);
+    }
 }
