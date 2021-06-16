ef9d65a899ceb5b842ef00b097110455c87448c7
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-06-16 22:42:12 +0900

    refactor: CodeEnumTypeHandler 설명 추가 및 관련 Exception 생성

--- src/main/java/com/flab/demo/enums/CodeEnumTypeHandler.java
+++ src/main/java/com/flab/demo/enums/CodeEnumTypeHandler.java
@@ -1,6 +1,7 @@
 package com.flab.demo.enums;
 
 import com.flab.demo.enums.CodeEnum;
+import com.flab.demo.exception.common.UnnownEnumValueException;
 import com.flab.demo.exception.member.UnknownRoleValueException;
 import lombok.NoArgsConstructor;
 import org.apache.ibatis.type.JdbcType;
@@ -13,8 +14,18 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Optional;
 
+/**
+ * Enum(Java) ↔ int(DB)와 같이 자바 타입과 JDBC 타입이 일치하지 않는 경우
+ * MyBatis는 ResultSet에서 값을 가져올때마다 적절한 자바 타입의 값을 가져오기 위해 `TypeHandler`를 사용합니다.
+ *
+ * 표준 자바 타입의 경우 사용할 수 있는 디폴트 TypeHandlers가 있으나
+ * 비표준인 Enum class 오브젝트를 사용하기 때문에 TypeHandler 인터페이스를 오버라이드하여 사용합니다.
+ *
+ * 이러한 Enum 클래스마다 TypeHandler 인터페이스를 구현하는 것은 파일의 수도 많아지고 관리가 힘들어지기 때문에
+ * 해당 이슈가 있는 Enum 클래스에 대한 공통 인터페이스 CodeEnum과 제네릭으로 범용적인 사용가능한 CodeEnumTypeHandler를 사용합니다.
+ */
 @NoArgsConstructor
-public class CodeEnumTypeHandler<E extends Enum<E>> implements TypeHandler<CodeEnum> {
+public class CodeEnumTypeHandler<E extends CodeEnum> implements TypeHandler<CodeEnum> {
 
     public Class<E> type;
 
@@ -48,6 +59,6 @@ public class CodeEnumTypeHandler<E extends Enum<E>> implements TypeHandler<CodeE
     private CodeEnum getEnum(int code) {
         Optional<CodeEnum> anyEnum = Arrays.stream((CodeEnum[]) type.getEnumConstants())
                 .filter(anEnum -> anEnum.getCode() == code).findAny();
-        return anyEnum.orElseThrow(() -> new UnknownRoleValueException(code));
+        return anyEnum.orElseThrow(() -> new UnnownEnumValueException(type.getSimpleName(), code));
     }
 }
