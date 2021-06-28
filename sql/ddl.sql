ce5ab5303a532b9d447f8879f9702e3b669ed639
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-06-28 14:03:13 +0900

    add: 상품관련 Domain, Dto, Controller, table 생성

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -7,3 +7,17 @@ create table MEMBER_INFO (
  tinyint role DEFAULT 3,
  INDEX (email)
 )engine=InnoDB default character set = utf8;
+
+CREATE TABLE product (
+ id BIGINT AUTO_INCREMENT PRIMARY KEY,
+ product_name VARCHAR(50) NOT NULL,
+ fixed_price INT NOT NULL,
+ quantity INT NOT NULL,
+ sales_count INT NOT NULL,
+ seller_id BIGINT NOT NULL,
+ FOREIGN KEY(seller_id)
+ REFERENCES member_info(id) ON DELETE CASCADE,
+ sales_yn CHAR(1) NOT NULL DEFAULT 'Y',
+ create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+ INDEX (product_name)
+) engine=InnoDB default character set = utf8;
---
+++ src/main/java/com/flab/demo/domain/Product.java
@@ -0,0 +1,20 @@
+package com.flab.demo.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Builder
+@Getter
+public class Product {
+
+    private Long id;
+    private String productName;
+    private int fixedPrice;
+    private int quantity;
+    private int salesCount;
+    private Long sellerId;
+    private String salesYn;
+    private Timestamp createDate;
+}
