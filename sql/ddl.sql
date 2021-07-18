3ee4ee9f1c3fdcaca33b51dae6e30a0135f13dd9
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-07-18 17:28:55 +0900

    add: CategoryMapper 생성

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -22,9 +22,9 @@ CREATE TABLE product_detail (
 ) engine=InnoDB default character set = utf8;
 
 CREATE TABLE category (
- id INT AUTO_INCREMENT PRIMARY KEY,
- parent_id INT,
- category_name VARCHAR(30),
- FOREIGN KEY(parent_id) REFERENCES category(id),
- create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    parent_id INT,
+    category_name VARCHAR(30),
+    FOREIGN KEY(parent_id) REFERENCES category(id),
+    create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 ) engine=InnoDB default character set = utf8;
--- src/main/java/com/flab/demo/mapper/CategoryMapper.java
+++ src/main/java/com/flab/demo/mapper/CategoryMapper.java
@@ -1,9 +1,16 @@
 package com.flab.demo.mapper;
 
+import com.flab.demo.domain.Category;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface CategoryMapper {
 
+    int register(Category category);
 
+    Optional<Category> getById(int id);
+
+    void modifyProduct(Category category);
 }
