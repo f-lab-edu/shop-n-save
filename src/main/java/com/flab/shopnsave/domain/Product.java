72e2ac05a07048a8d76504d1e21bd7c312499cb7
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-07-25 20:02:46 +0900

    refactor: import 정리

--- src/main/java/com/flab/shopnsave/domain/Product.java
+++ src/main/java/com/flab/shopnsave/domain/Product.java
@@ -1,7 +1,10 @@
 package com.flab.shopnsave.domain;
 
 import com.flab.shopnsave.dto.product.ModifyProductRequestDto;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
