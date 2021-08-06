8b6b6b999b03e876b49596b1e9770fd409687e1f
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-06 17:01:44 +0900

    refactor: Member, AuthMember 클래스 리팩토링

--- src/main/java/com/flab/shopnsave/member/domain/Member.java
+++ src/main/java/com/flab/shopnsave/member/domain/Member.java
@@ -1,13 +1,16 @@
 package com.flab.shopnsave.member.domain;
 
 import com.flab.shopnsave.enums.Role;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 import java.sql.Timestamp;
 
-@Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
 
     private Long id;
@@ -17,6 +20,22 @@ public class Member {
     private String address;
     private Timestamp createDate;
     private Role role;
+    private String phone;
+
+    @Builder
+    public Member(String email, String password, String name, String address, String phone) {
+        Assert.hasText(email, "이메일이 존재하지 않습니다");
+        Assert.hasText(password, "비밀번호가 존재하지 않습니다");
+        Assert.hasText(name, "이름이 존재하지 않습니다");
+        Assert.hasText(address, "주소가 존재하지 않습니다");
+        Assert.hasText(phone, "전화번호가 존재하지 않습니다");
+
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 
     public void changePassword(String password) {
         this.password = password;
@@ -28,5 +47,9 @@ public class Member {
 
     public void changeAddress(String address) {
         this.address = address;
+    }
+
+    public void changePhone(String phone) {
+        this.phone = phone;
     }
 }
